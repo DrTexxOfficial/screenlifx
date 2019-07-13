b0200cccafa93ba075676f0ee16856c1bef07257
DrTexxOfficial <23412100+DrTexxOfficial@users.noreply.github.com>
2019-07-13 23:48:44 +1000

    replace ImageGrab with mss and implement supporting code

--- screenlifx.py
+++ screenlifx.py
@@ -7,9 +7,20 @@
 # https://github.com/mclarkk/lifxlan
 from lifxlan import * #
 
-from PIL import ImageGrab
+from mss import mss
 from PIL import Image
 import time, os, colorsys
+
+# preferences
+factor = 0.75
+
+# split image filename into name and extension
+#name, ext = os.path.splitext(image_file)
+
+# create a new file name for saving the result
+#new_image_file = "%s%s%s" % (name, str(factor), ext)
+#img_anti.save(new_image_file)
+#print("resized file saved as %s" % new_image_file)
 
 # helper for colors conversion/scaling
 def rgb2hsv(r, g, b):
@@ -37,7 +48,23 @@ def main():
 
     # screen scanning loop
     while True:
-        img = ImageGrab.grab()
+
+        with mss() as sct: image_file = sct.shot()
+
+        img_org = Image.open(image_file)
+
+        width_org, height_org = img_org.size
+
+        width = int(width_org * factor)
+        height = int(height_org * factor)
+
+        # best down-sizing filter
+        #img_resized = img_org.resize((width, height), Image.ANTIALIAS)
+        # quickest down-sizing filter
+        img_resized = img_org.resize((width, height), Image.NEAREST)
+
+        img = img_resized
+
         t1 = time.process_time()
         # let PIL to shrink the image into a more manageable size
         # (just few ms in your average machine)
